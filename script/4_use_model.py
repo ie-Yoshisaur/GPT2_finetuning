4505e826cd33c9ca306ce9089969dcaf1dbf88fb
e205723 <e205723@ie.u-ryukyu.ac.jp>
2021-12-19 17:54:39 +0900

    change tokenizer: AutoTokenizer -> T5Tokenizer

--- script/4_use_model.py
+++ script/4_use_model.py
@@ -14,17 +14,9 @@ model.eval()
 
 def generate_reply(inp, num_gen=1):
     input_text = "<s>" + str(inp) + "[SEP]"
-    input_ids = tokenizer.encode(input_text, return_tensors='pt').to(device)
-    out = model.generate(input_ids, do_sample=True, max_length=128, num_return_sequences=num_gen, top_p=0.95, top_k=50, bad_words_ids=[[1], [5]], no_repeat_ngram_size=3)
-
-    print(">", "You")
-    print(inp)
-    print(">", "Kono")
-    for sent in tokenizer.batch_decode(out):
-        sent = sent.split('[SEP]</s>')[1]
-        sent = sent.replace('</s>', '')
-        sent = sent.replace('<br>', '\n')
-        print(sent)
+    input = tokenizer.encode(input_text, return_tensors='pt')
+    output = model.generate(input, do_sample=True, max_length=128, num_return_sequences=num_gen, top_p=0.95, top_k=50, bad_words_ids=[[1], [5]], no_repeat_ngram_size=3)
+    print(tokenizer.batch_decode(output))
 
 while(True):
     msg = input()
