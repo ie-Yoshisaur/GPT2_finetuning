79d2de41d0129b4deb9498e6eebc71ae00fd1f38
e205723 <e205723@ie.u-ryukyu.ac.jp>
2022-01-13 08:00:15 +0900

    fix(requirements.txt): change the version of sentencepiece

--- shell_script/1_setup.sh
+++ shell_script/1_setup.sh
@@ -1,4 +1,5 @@
-rm -rf transformers output/*
+rm -rf transformers output
+mkdir output
 git clone -b v4.4.2-release https://github.com/huggingface/transformers/
 rm ./transformers/examples/language-modeling/run_clm.py
 cp run_clm.py ./transformers/examples/language-modeling/run_clm.py
